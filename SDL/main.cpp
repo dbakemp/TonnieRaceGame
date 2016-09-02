1c24badb300448bb34472b9cb6307c843d795d18
Delroy van der Kemp <delroystar@hotmail.com>
2016-09-02 20:44:45 +0200

    Initial Commit

---
+++ SDL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL
+//
+//  Created by Delroy van der Kemp on 02/09/2016.
+//  Copyright © 2016 42IN13SAg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
