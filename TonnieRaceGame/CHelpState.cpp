44f32ded5984a3f07b5ab1c817d53860f57cae51
Timmeehhh <timvbeers@icloud.com>
2016-11-27 21:42:12 +0100

    Image downloaden

--- TonnieRaceGame/CHelpState.cpp
+++ TonnieRaceGame/CHelpState.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include "SDL.h"
 #include "SDL_image.h"
 #include "SDL_ttf.h"
@@ -7,15 +8,19 @@
 #include "CDrawManager.h"
 #include "CInputManager.h"
 #include <iostream>
-
+#include <curl/curl.h>
 void CHelpState::init()
 {
 	return;
 }
 
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+	size_t written = fwrite(ptr, size, nmemb, stream);
+	return written;
+}
+
 void CHelpState::init(CEngine* engine)
 {
-
 	SDL_Surface* background = IMG_Load("Resources/Images/help.jpg");
 	SDL_Texture* background_texture = SDL_CreateTextureFromSurface(engine->renderer, background);
 
@@ -29,6 +34,51 @@ void CHelpState::init(CEngine* engine)
 	SDL_RenderPresent(engine->renderer);
 
 	engine->musicHelper->playTrack("music\\plingplongplong.mp3", false);
+
+	CURL *curl;
+	FILE *fp;
+	CURLcode res;
+
+	FILE *file;
+	int file_exists;
+	const char * filename = "ads\\my_file.png";
+
+	/*first check if the file exists...*/
+	file = fopen(filename, "r");
+	if (file == NULL) file_exists = 0;
+	else { file_exists = 1; fclose(file); }
+
+	/*...then open it in the appropriate way*/
+	if (file_exists == 1)
+	{
+		printf("file exists!\n");
+		file = fopen(filename, "r+b");
+	}
+	else
+	{
+		printf("file does not exist!\n");
+		file = fopen(filename, "w+b");
+	}
+
+	if (file != NULL)
+	{
+		printf("file opened succesfully!\n");
+		fclose(file);
+	}
+
+	char *url = "http://localhost/marketing/uploads/20161127181139.png";
+	//char outfilename[FILENAME_MAX] = "C:\Users\Tim\Desktop\Desktop\School\bbb.png";
+	curl = curl_easy_init();
+	if (curl) {
+		fp = fopen(filename, "wb");
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+		res = curl_easy_perform(curl);
+		/* always cleanup */
+		curl_easy_cleanup(curl);
+		fclose(fp);
+	}
 }
 
 void CHelpState::clean()
