0a3b2a51ff5620de6f7edcb1a6839da6de73d449
Timmeehhh <timvbeers@icloud.com>
2016-12-01 17:15:11 +0100

    sorry ik had verkloot

--- TonnieRaceGame/CAdManager.cpp
+++ TonnieRaceGame/CAdManager.cpp
@@ -29,16 +29,6 @@ void CAdManager::AddAd(Json::Value * ad)
 {
 	std::string location = "ads/" + (*ad).get("FileName", "").asString();
 	SDL_Surface* texture = IMG_Load(location.c_str());
-	SDL_Texture *text = IMG_LoadTexture(engine->renderer, location.c_str());
-
-	float angle = 180.0f; // set the angle.
-	SDL_Point center = { 8, 8 }; // the center where the texture will be rotated.
-	SDL_RendererFlip flip = SDL_FLIP_NONE; // the flip of the texture.
-
-										   // now, try to draw something
-	SDL_RenderCopyEx(engine->renderer, text, NULL, NULL, angle, &center, flip);
-	std::cout << "flipie";
-	texture = (SDL_Surface*)text;
 
 	textureMap.insert(std::pair<std::string, SDL_Texture*>((*ad).get("Title", "").asString(), SDL_CreateTextureFromSurface(engine->renderer, texture)));
 	mapIndexes.push_back((*ad).get("Title", "").asString());
